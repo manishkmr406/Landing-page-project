9550284440e0806417f487783dd9ffe71e7776b8
Manishkmr406 <manishkmr406@gmail.com>
2023-08-24 19:23:11 +0530

    buttons toggle content updated

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ bars.addEventListener('click',()=>{
     const nav=document.querySelector('.nav-links');
     nav.classList.toggle('active-nav');
 })
+
+
+const selectButtons=document.querySelectorAll('.selectbtn');
+const contents=document.querySelectorAll(".content");
+
+contents[0].style.display= 'flex';
+selectButtons.forEach((button,index)=>{
+    button.addEventListener('click',()=>{
+        contents.forEach((ctx,ctxIndex)=>{
+         if(index===ctxIndex){
+            ctx.style.display= 'flex';
+            button.focus();
+         }else{
+            ctx.style.display='none';
+         }
+        })
+    })
+})
